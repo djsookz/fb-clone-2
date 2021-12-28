e749784d900aead6080e116d968cc39f0968dded
DjSookz <65423860+djsookz@users.noreply.github.com>
2021-12-28 13:31:35 +0200

    update the pic of input

--- components/InputBox.js
+++ components/InputBox.js
@@ -73,13 +73,15 @@ function InputBox() {
   return (
     <div className="bg-white p-2 rounded-2xl shadow-md text-gray-500 font-medium mt-6">
       <div className="flex space-x-2 p-1 items-center">
-        <Image
-          className="rounded-full"
-          src={session.user.image}
-          width={40}
-          height={40}
-          layout="fixed"
-        />
+        <div className="pb-1">
+          <Image
+            className="rounded-full"
+            src={session.user.image}
+            width={42}
+            height={42}
+            layout="fixed"
+          />
+        </div>
         <form className="flex flex-1 pb-2">
           <input
             className="rounded-full h-12 bg-gray-100 flex-grow px-5 focus:outline-none"
