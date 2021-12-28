01e6f053f8b5bfe796d5190d4b614962bf97d69c
DjSookz <65423860+djsookz@users.noreply.github.com>
2021-12-28 13:07:48 +0200

    update border of icons

--- components/HeaderIcon.js
+++ components/HeaderIcon.js
@@ -1,6 +1,10 @@
 function HeaderIcon({ Icon, active }) {
   return (
-    <div className="relative flex  items-center cursor-pointer md:px-5 lg:px-10 sm:h-14 sm:ml-14 md:hover:bg-gray-100  rounded-xl active:border-b-2 active:border-blue-500 group">
+    <div
+      className={`relative flex  items-center cursor-pointer md:px-5 lg:px-10 sm:h-14 sm:ml-14 ${
+        active && "md:hover:bg-white active:border-b-0"
+      } md:hover:bg-gray-100  rounded-xl active:border-b-2 active:border-blue-500 group `}
+    >
       <Icon
         className={`text-lg text-gray-500 text-center sm:h-7 mx-auto group-hover:text-blue-500 ${
           active && "text-blue-500  "
